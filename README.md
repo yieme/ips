4bd2e214e4eebfde01ab5bdc12bd59bf7033f98a
yieme <yieme@cormea.com>
2015-02-05 11:38:30 -0700

    initial

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# ips
+# ips <img src="https://raw.githubusercontent.com/voodootikigod/logo.js/master/js.png" align="right" height="120">\index
 Get local, docker and public IP addresses
+
+# Install
+
+
+```js
+npm install ips
+```
+
+# License: MIT
---
+++ index.js
@@ -0,0 +1,17 @@
+// ips - Get local, docker and public IP addresses - Copyright (C) 2015 by yieme - All Rights Reserved - License: MIT
+
+;(function() {
+  'use strict';
+
+  // var convar = require('convar')
+
+  var Ips = function() {
+
+  }
+
+
+
+  if (typeof exports === 'object') module.exports = Ips // support CommonJS
+  // else if (typeof define === 'function' && define.amd) define(function() { return Ips }) // support AMD
+  // else this.Ips = Ips // support browser
+})();
